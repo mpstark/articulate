6628a49aff2501e6b7cc8aeb448539da149fe139
Michael Starkweather <Mpstark@gmail.com>
2013-10-04 13:20:03 -0500

    Some code cleanup.
    * Focused on some simple classes at first.

--- Articulate/Components/VoiceRecognizer.cs
+++ Articulate/Components/VoiceRecognizer.cs
@@ -10,11 +10,26 @@ using System.Diagnostics;
 
 namespace Articulate
 {
+    /// <summary>
+    /// Object that listens for user voice input (as setup by the CommandPool) and then passes the execution to CommandPool.
+    /// </summary>
     class VoiceRecognizer : IDisposable
     {
+        /// <summary>
+        /// Ugly way of checking if the voiceEngine was setup.
+        /// </summary>
         public bool IsSetup = false;
-        SpeechRecognitionEngine voiceEngine;
 
+        /// <summary>
+        /// The voice recognition engine.
+        /// </summary>
+        private SpeechRecognitionEngine voiceEngine;
+
+        /// <summary>
+        /// Default constructor. Sets up the voice recognizer with default settings.
+        /// 
+        /// Namely, default options are: en-US, default input device, listen always.
+        /// </summary>
         public VoiceRecognizer()
         {
             try
@@ -28,9 +43,7 @@ namespace Articulate
                 // Create the Grammar instance and load it into the speech recognition engine.
                 Grammar g = new Grammar(CommandPool.BuildSrgsGrammar());
                 voiceEngine.LoadGrammar(g);
-
-                //voiceEngine.EndSilenceTimeout = new TimeSpan(0, 0, 1);
-
+                
                 // Register a handler for the SpeechRecognized event
                 voiceEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
 
@@ -40,23 +53,35 @@ namespace Articulate
             }
             catch(Exception e)
             {
+                // Something went wrong setting up the voiceEngine.
+                Trace.WriteLine(e.Message);
                 IsSetup = false;
             }
         }
 
-        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        /// <summary>
+        /// Some speech was recognized by the voiceEngine.
+        /// </summary>
+        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs recognizedPhrase)
         {
-            if (e.Result.Confidence > .90)
+            if (recognizedPhrase.Result.Confidence > .90)
             {
-                // Async deal with it
-                Task.Factory.StartNew(() => CommandPool.Execute(e.Result.Semantics));
+                // Get a thread from the thread pool to deal with it
+                Task.Factory.StartNew(() => CommandPool.Execute(recognizedPhrase.Result.Semantics));
             }
         }
 
+        /// <summary>
+        /// Cleanup before destoying the object.
+        /// </summary>
         public void Dispose()
         {
+            // make sure that the voiceEngine is turned off
             voiceEngine.RecognizeAsyncCancel();
+
+            // dispose of the voiceEngine
             voiceEngine.Dispose();
+            voiceEngine = null;
         }
     }
 }
