5b47106c59c3383487a734824695ba58406effe4
Benjamin Pannell <admin@sierrasoftworks.com>
2013-10-05 21:18:28 +0200

    Add settings file storage

---
+++ Articulate/Settings/Settings.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Xml.Serialization;
+using System.Diagnostics;
+
+namespace Articulate
+{
+	[Serializable]
+	public class Settings
+	{
+		public Settings()
+		{
+			// Initialize default settings
+			ConfidenceMargin = 80;
+
+			PTTKey = System.Windows.Forms.Keys.None;
+		}
+
+		#region File Handling
+
+		public static Settings Load()
+		{
+			var filePath = Environment.ExpandEnvironmentVariables(@"%AppData%\Articulate\Settings.xml");
+			if (!File.Exists(filePath)) return new Settings();
+
+			try
+			{
+				using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+				{
+					var serializer = new XmlSerializer(typeof(Settings));
+					return (Settings)serializer.Deserialize(fs);
+				}
+			}
+			catch (Exception ex)
+			{
+				Trace.Write(ex.Message);
+				return new Settings();
+			}
+		}
+
+		public void Save()
+		{
+			var filePath = Environment.ExpandEnvironmentVariables(@"%AppData%\Articulate\Settings.xml");
+						
+			try
+			{
+				var parentDirectory = Environment.ExpandEnvironmentVariables(@"%AppData%\Articulate");
+				if (!Directory.Exists(parentDirectory))
+					Directory.CreateDirectory(parentDirectory);
+
+				using (var fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+				{
+					var serializer = new XmlSerializer(typeof(Settings));
+					serializer.Serialize(fs, this);
+				}
+			}
+			catch(Exception ex)
+			{
+				Trace.Write(ex.Message);
+			}
+		}
+
+		#endregion
+
+		public int ConfidenceMargin
+		{ get; set; }
+
+		public System.Windows.Forms.Keys PTTKey
+		{ get; set; }
+
+		public bool PushToIgnore
+		{ get; set; }
+	}
+}
