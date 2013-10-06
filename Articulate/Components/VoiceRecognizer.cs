0eb20ab748d9cf4d6d642fb31a7ac71d1cdc12c7
Benjamin Pannell <admin@sierrasoftworks.com>
2013-10-06 03:08:20 +0200

    Tabify VoiceRecognizer

--- Articulate/Components/VoiceRecognizer.cs
+++ Articulate/Components/VoiceRecognizer.cs
@@ -14,32 +14,32 @@ namespace Articulate
 	/// Object that listens for user voice input (as setup by the CommandPool) and then passes the execution to CommandPool.
 	/// </summary>
 	class VoiceRecognizer : IDisposable
-    {
-        #region Private Members
-        /// <summary>
-        /// Microsoft's in process speech recognition.
-        /// </summary>
-        private SpeechRecognitionEngine Engine { get; set; }
+	{
+		#region Private Members
+		/// <summary>
+		/// Microsoft's in process speech recognition.
+		/// </summary>
+		private SpeechRecognitionEngine Engine { get; set; }
 
-        private Object ConfidenceLock;
-        #endregion
+		private Object ConfidenceLock;
+		#endregion
 
-        #region Public Members
-        /// <summary>
-        /// Enum that exposes the state of the VoiceRecognizer
-        /// </summary>
-        public enum VoiceRecognizerState
-        {
-            Error,
-            Listening,
-            ListeningOnce,
-            Paused,
-        }
+		#region Public Members
+		/// <summary>
+		/// Enum that exposes the state of the VoiceRecognizer
+		/// </summary>
+		public enum VoiceRecognizerState
+		{
+			Error,
+			Listening,
+			ListeningOnce,
+			Paused,
+		}
 
-        /// <summary>
-        /// Ugly way of providing VoiceRecognizer status
-        /// </summary>
-        public VoiceRecognizerState State
+		/// <summary>
+		/// Ugly way of providing VoiceRecognizer status
+		/// </summary>
+		public VoiceRecognizerState State
 		{
 			get;
 			private set;
@@ -47,158 +47,158 @@ namespace Articulate
 
 		public List<string> MonitoredExecutables = new List<string>();
 		
-        /// <summary>
-        /// Ugly way of providing exception message.
-        /// </summary>
-        public string SetupError
-        {
-            get;
-            private set;
-        }
+		/// <summary>
+		/// Ugly way of providing exception message.
+		/// </summary>
+		public string SetupError
+		{
+			get;
+			private set;
+		}
 
-        /// <summary>
-        /// The level at which speech is rejected.
-        /// </summary>
+		/// <summary>
+		/// The level at which speech is rejected.
+		/// </summary>
 		public int ConfidenceMargin
 		{
 			get { return Engine != null ? (int)Engine.QueryRecognizerSetting("CFGConfidenceRejectionThreshold") : 90; }
 			set
 			{                
 				if(Engine != null)
-                {
+				{
 					ChangeConfidence(value);
 
-                    // TODO: this is not FIFO 
-                    // roll off a thread to set it
-                    //Task.Factory.StartNew(() => ChangeConfidence(value));
-                }
-                else
-                {
-                    Trace.WriteLine("Can't change confidence");
-                }
+					// TODO: this is not FIFO 
+					// roll off a thread to set it
+					//Task.Factory.StartNew(() => ChangeConfidence(value));
+				}
+				else
+				{
+					Trace.WriteLine("Can't change confidence");
+				}
 			}
 		}
-        #endregion
+		#endregion
 
-        #region Constructor
-        /// <summary>
+		#region Constructor
+		/// <summary>
 		/// Default constructor. Sets up the voice recognizer with default settings.
 		/// 
 		/// Namely, default options are: en-US, default input device, listen always, confidence level at .90
 		/// </summary>
 		public VoiceRecognizer()
 		{
-            try
-            {
-                ConfidenceLock = new Object();
-                State = VoiceRecognizerState.Paused;
-                CultureInfo cultureInfo = new CultureInfo("en-US");
+			try
+			{
+				ConfidenceLock = new Object();
+				State = VoiceRecognizerState.Paused;
+				CultureInfo cultureInfo = new CultureInfo("en-US");
 
 				// Create a new SpeechRecognitionEngine instance.
-                Engine = new SpeechRecognitionEngine(cultureInfo);
-                
-                try
-                {
-                    // Setup the audio device
-                    Engine.SetInputToDefaultAudioDevice();
-                }
-                catch (InvalidOperationException ex)
-                {
-                    // No default input device
-                    Trace.WriteLine(ex.Message);
-                    SetupError = "Check input device.\n\n";
-                    State = VoiceRecognizerState.Error;
-                    return;
-                }
+				Engine = new SpeechRecognitionEngine(cultureInfo);
+				
+				try
+				{
+					// Setup the audio device
+					Engine.SetInputToDefaultAudioDevice();
+				}
+				catch (InvalidOperationException ex)
+				{
+					// No default input device
+					Trace.WriteLine(ex.Message);
+					SetupError = "Check input device.\n\n";
+					State = VoiceRecognizerState.Error;
+					return;
+				}
 
-                // Set the confidence setting
-                ConfidenceMargin = 90;
+				// Set the confidence setting
+				ConfidenceMargin = 90;
 
 				// Create the Grammar instance and load it into the speech recognition engine.
-                Grammar g = new Grammar(CommandPool.BuildSrgsGrammar(cultureInfo));
+				Grammar g = new Grammar(CommandPool.BuildSrgsGrammar(cultureInfo));
 				Engine.LoadGrammar(g);
 
 				// Register a handler for the SpeechRecognized event
 				Engine.SpeechRecognized += sre_SpeechRecognized;
 				Engine.SpeechRecognitionRejected += sre_SpeechRecognitionRejected;
 
-                StartListening();
+				StartListening();
 			}
 			catch(Exception ex)
 			{
 				// Something went wrong setting up the voiceEngine.
-                Trace.WriteLine(ex.Message);
-                SetupError = ex.ToString();
+				Trace.WriteLine(ex.Message);
+				SetupError = ex.ToString();
 				State = VoiceRecognizerState.Error;
 			}
 		}
-        #endregion
+		#endregion
 
-        #region Public Methods
-        /// <summary>
-        /// Starts Async listening.
-        /// </summary>
-        public void StartListening()
-        {
-            if (Engine != null && State == VoiceRecognizerState.Paused)
-            {
-                // Start listening in multiple mode (that is, don't quit after a single recongition)
-                Engine.RecognizeAsync(RecognizeMode.Multiple);
-                State = VoiceRecognizerState.Listening;
-            }
-        }
+		#region Public Methods
+		/// <summary>
+		/// Starts Async listening.
+		/// </summary>
+		public void StartListening()
+		{
+			if (Engine != null && State == VoiceRecognizerState.Paused)
+			{
+				// Start listening in multiple mode (that is, don't quit after a single recongition)
+				Engine.RecognizeAsync(RecognizeMode.Multiple);
+				State = VoiceRecognizerState.Listening;
+			}
+		}
 
-        /// <summary>
-        /// Stops Async listening gracefully.
-        /// </summary>
-        public void StopListening()
-        {
-            if (Engine != null && (State == VoiceRecognizerState.Listening || State == VoiceRecognizerState.ListeningOnce))
-            {
-                // Stop listening gracefully
-                Engine.RecognizeAsyncStop();
-                State = VoiceRecognizerState.Paused;
-            }
-        }
+		/// <summary>
+		/// Stops Async listening gracefully.
+		/// </summary>
+		public void StopListening()
+		{
+			if (Engine != null && (State == VoiceRecognizerState.Listening || State == VoiceRecognizerState.ListeningOnce))
+			{
+				// Stop listening gracefully
+				Engine.RecognizeAsyncStop();
+				State = VoiceRecognizerState.Paused;
+			}
+		}
 
-        /// <summary>
-        /// Stops Async listening immediately.
-        /// </summary>
-        public void CancelListening()
-        {
-            if (Engine != null && (State == VoiceRecognizerState.Listening || State == VoiceRecognizerState.ListeningOnce))
-            {
-                Engine.RecognizeAsyncCancel();
-                State = VoiceRecognizerState.Paused;
-            }
-        }
+		/// <summary>
+		/// Stops Async listening immediately.
+		/// </summary>
+		public void CancelListening()
+		{
+			if (Engine != null && (State == VoiceRecognizerState.Listening || State == VoiceRecognizerState.ListeningOnce))
+			{
+				Engine.RecognizeAsyncCancel();
+				State = VoiceRecognizerState.Paused;
+			}
+		}
 
-        /// <summary>
-        /// Listens for a single utterance and then stops
-        /// </summary>
-        public void ListenOnce()
-        {
-            if (Engine != null && State == VoiceRecognizerState.Paused)
-            {
-                // only listen for a single utterance
-                Engine.RecognizeAsync(RecognizeMode.Single);
-                State = VoiceRecognizerState.ListeningOnce;
-            }
-        }
-        #endregion
+		/// <summary>
+		/// Listens for a single utterance and then stops
+		/// </summary>
+		public void ListenOnce()
+		{
+			if (Engine != null && State == VoiceRecognizerState.Paused)
+			{
+				// only listen for a single utterance
+				Engine.RecognizeAsync(RecognizeMode.Single);
+				State = VoiceRecognizerState.ListeningOnce;
+			}
+		}
+		#endregion
 
-        #region Private Methods
-        private void ChangeConfidence(int value)
-        {
-            lock (ConfidenceLock)
-            {
-                Engine.UpdateRecognizerSetting("CFGConfidenceRejectionThreshold", value);
-            }
-        }
-        #endregion
+		#region Private Methods
+		private void ChangeConfidence(int value)
+		{
+			lock (ConfidenceLock)
+			{
+				Engine.UpdateRecognizerSetting("CFGConfidenceRejectionThreshold", value);
+			}
+		}
+		#endregion
 
-        #region SpeechRecognitionEngine Events
-        /// <summary>
+		#region SpeechRecognitionEngine Events
+		/// <summary>
 		/// Some speech was recognized by the voiceEngine.
 		/// </summary>
 		private void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs recognizedPhrase)
@@ -224,10 +224,10 @@ namespace Articulate
 		{
 			Trace.WriteLine("Rejected with confidence: " + recognizedPhrase.Result.Confidence);
 		}
-        #endregion
+		#endregion
 
-        #region IDispose Implementation
-        /// <summary>
+		#region IDispose Implementation
+		/// <summary>
 		/// Cleanup before destoying the object.
 		/// </summary>
 		public void Dispose()
@@ -238,7 +238,7 @@ namespace Articulate
 			// dispose of the voiceEngine
 			Engine.Dispose();
 			Engine = null;
-        }
-        #endregion
-    }
+		}
+		#endregion
+	}
 }
