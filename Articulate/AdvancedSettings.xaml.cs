6a9d0864e682b3c3642f16c2ad6c954355c5437a
Benjamin Pannell <admin@sierrasoftworks.com>
2013-10-09 16:59:34 +0200

    Add basic advanced settings window template

---
+++ Articulate/AdvancedSettings.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Articulate
+{
+	/// <summary>
+	/// Interaction logic for AdvancedSettings.xaml
+	/// </summary>
+	public partial class AdvancedSettings : MetroWindow
+	{
+		public AdvancedSettings(Settings settings)
+		{
+			Settings = settings;
+
+			InitializeComponent();
+		}
+
+		public Settings Settings
+		{ get; private set; }
+
+		void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		void Window_Closing(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
