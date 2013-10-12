3684204a5f3ece82da34e5ab99069fccf48a0e08
Benjamin Pannell <admin@sierrasoftworks.com>
2013-10-12 11:36:19 +0200

    Fix Access Denied in ForegroundProcess (fixes #36)
    Makes use of a Vista+ API which provides partial information access to
    restricted processes querying elevated ones. Should solve the issue
    Alpha-Kilo was experiencing.

--- Articulate/Components/ForegroundProcess.cs
+++ Articulate/Components/ForegroundProcess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -20,6 +21,71 @@ namespace Articulate
 		[DllImport("user32.dll")]
 		static extern IntPtr GetForegroundWindow();
 
+
+		#region Native API
+
+		[Flags]
+		enum ProcessAccessFlags : uint
+		{
+			All = 0x001F0FFF,
+			Terminate = 0x00000001,
+			CreateThread = 0x00000002,
+			VMOperation = 0x00000008,
+			VMRead = 0x00000010,
+			VMWrite = 0x00000020,
+			DupHandle = 0x00000040,
+			SetInformation = 0x00000200,
+			QueryInformation = 0x00000400,
+			QueryLimitedInformation = 0x1000,
+			Synchronize = 0x00100000
+		}
+
+		private static string GetExecutablePath(Process Process)
+		{
+			//If running on Vista or later use the new function
+			if (Environment.OSVersion.Version.Major >= 6)
+			{
+				return GetExecutablePathAboveVista(Process.Id);
+			}
+
+			return Process.MainModule.FileName;
+		}
+
+		private static string GetExecutablePathAboveVista(int ProcessId)
+		{
+			var buffer = new StringBuilder(1024);
+			IntPtr hprocess = OpenProcess(ProcessAccessFlags.QueryLimitedInformation,
+										  false, ProcessId);
+			if (hprocess != IntPtr.Zero)
+			{
+				try
+				{
+					int size = buffer.Capacity;
+					if (QueryFullProcessImageName(hprocess, 0, buffer, out size))
+					{
+						return buffer.ToString();
+					}
+				}
+				finally
+				{
+					CloseHandle(hprocess);
+				}
+			}
+			throw new Win32Exception(Marshal.GetLastWin32Error());
+		}
+
+		[DllImport("kernel32.dll")]
+		private static extern bool QueryFullProcessImageName(IntPtr hprocess, int dwFlags,
+					   StringBuilder lpExeName, out int size);
+		[DllImport("kernel32.dll")]
+		private static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess,
+					   bool bInheritHandle, int dwProcessId);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		private static extern bool CloseHandle(IntPtr hHandle);
+
+		#endregion
+
 		/// <summary>
 		/// Gets the Full Path to the current foreground window's executable file
 		/// </summary>
@@ -30,8 +96,10 @@ namespace Articulate
 				IntPtr processID;
 				GetWindowThreadProcessId(GetForegroundWindow(), out processID);
 
+				if (Environment.OSVersion.Version.Major >= 6)				
+					return GetExecutablePathAboveVista((int)processID);
+				
 				Process p = Process.GetProcessById((int)processID);
-
 				return p.MainModule.FileName;
 			}
 		}
