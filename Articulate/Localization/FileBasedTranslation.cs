0868ef9c253363de79567e1ca50eed775ccf000b
Benjamin Pannell <admin@sierrasoftworks.com>
2013-10-23 21:28:02 +0200

    Move FileBasedTranslation to SierraLib.Translation Motivated by the wider applicability of this class to projects other than Articulate.

--- Articulate/Localization/FileBasedTranslation.cs
+++ Articulate/Localization/FileBasedTranslation.cs
@@ -7,179 +7,44 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Articulate
 {
-	public sealed class FileBasedTranslation : ITranslation
+	public sealed class FileBasedTranslation : SierraLib.Translation.FileBasedTranslation
 	{
 		public FileBasedTranslation(CultureInfo culture, Stream file)
+			: base(culture, file)
 		{
-			Culture = culture;
-			ProcessFile(file);
+
 		}
-
-		public System.Globalization.CultureInfo Culture
+		
+		public override string this[string key]
 		{
-			get;
-			private set;
-		}
-
-		public string Author
-		{
-			get;
-			private set;
-		}
-
-		private Dictionary<string, string> FileData = new Dictionary<string, string>();
-
-		public string this[string key]
-		{
-			get { return FileData.ContainsKey(key) ? FileData[key] : "!!!" + key; }
-		}
-
-		public string this[string key, string defaultValue]
-		{
-			get { return FileData.ContainsKey(key) ? FileData[key] : defaultValue; }
-		}
-
-		#region File Processing
-
-		enum LexerToken
-		{
-			Ignore,
-			BeginKey,
-			Key,
-			BeginValue,
-			Value,
-			Invalid
-		}
-
-		class Token
-		{
-			public Token()
+			get 
 			{
-				Type = LexerToken.BeginKey;
-				Value = "";
-			}
-
-			public LexerToken Type;
-			public string Value;
-			public Token Next;
-		}
-				
-		LexerToken NextToken(string content, ref int index, LexerToken currentToken)
-		{
-			switch (currentToken)
-			{
-				case LexerToken.BeginKey:
-					if (content[index] == '\r' || content[index] == '\n') return LexerToken.Ignore;
-					if (char.IsLetter(content, index) || content[index] == '_') return LexerToken.Key;
-					return LexerToken.Invalid;
-				case LexerToken.BeginValue:
-					if (char.IsWhiteSpace(content, index) || content[index] == '=') return LexerToken.Ignore;
-					return LexerToken.Value;
-				case LexerToken.Key:
-					if (char.IsLetterOrDigit(content, index) || content[index] == '_') return LexerToken.Key;
-					if (char.IsWhiteSpace(content, index) || content[index] == '=') return LexerToken.BeginValue;
-					return LexerToken.Invalid;
-				case LexerToken.Value:
-					if(content[index] == '\r') return LexerToken.Ignore;
-					if (content[index] == '\n')
-					{
-						if (content[index + 1] == '\t') return LexerToken.Ignore;
-						return LexerToken.BeginKey;
-					}
-					if (content[index] == '\t' && content[index - 1] == '\n') return LexerToken.Ignore;
-					return LexerToken.Value;
-				default: return LexerToken.Invalid;
+				return this[key, "!!!" + key];
 			}
 		}
 
-		void ProcessFile(Stream file)
+		public override string this[string key, string defaultValue]
 		{
-			var sr = new StreamReader(file);
-			var data = sr.ReadToEnd();
-			
-			var RootToken = new Token();
-			var ActiveToken = RootToken;
-
-			var ActiveStringBuilder = new StringBuilder();
-
-			for (var i = 0; i < data.Length; i++)
+			get 
 			{
-				var nextTokenType = NextToken(data, ref i, ActiveToken.Type);
-				
-				switch (nextTokenType)
+				if (FileData.ContainsKey(key)) return FileData[key];
+				else if (key.StartsWith("keyboard_"))
 				{
-					case LexerToken.Ignore:
-						continue;
-					case LexerToken.BeginKey:
-					case LexerToken.BeginValue:
-						ActiveToken.Value = ActiveStringBuilder.ToString();
-						ActiveStringBuilder.Clear();
-						ActiveToken = ActiveToken.Next = new Token()
-						{
-							Type = nextTokenType
-						};
-						break;
-					case LexerToken.Key:
-						if (ActiveToken.Type == LexerToken.BeginKey)
-						{
-							ActiveToken.Type = LexerToken.Key;
-							ActiveStringBuilder.Append(data[i]);
-						}
-						else if (ActiveToken.Type == LexerToken.Key)
-							ActiveStringBuilder.Append(data[i]);
-						else throw new InvalidOperationException("Invalid state transitions in translation file lexer, a key cannot appear after a " + ActiveToken.Type.ToString());
-						break;
-					case LexerToken.Value:
-						if (ActiveToken.Type == LexerToken.BeginValue)
-						{
-							ActiveToken.Type = LexerToken.Value;
-							ActiveStringBuilder.Append(data[i]);
-						}
-						else if (ActiveToken.Type == LexerToken.Value)
-							ActiveStringBuilder.Append(data[i]);
-						else throw new InvalidOperationException("Invalid state transitions in translation file lexer, a value cannot appear after a " + ActiveToken.Type.ToString());
-						break;
-					case LexerToken.Invalid:
-						throw new FileFormatException("Bad file format near '" + data.Substring(Math.Max(0, i - 10), Math.Min(20, data.Length - 20)) + "'");
+					var keyCode = Convert.ToInt32(key.Substring("keyboard_".Length));
+					return ((System.Windows.Forms.Keys)keyCode).ToString();
 				}
-			}
-
-			ActiveToken.Value = ActiveStringBuilder.ToString();
-
-			ActiveToken = RootToken;
-			while (ActiveToken != null)
-			{
-				if (ActiveToken.Type != LexerToken.Key)
-					throw new FileFormatException("Expected a Key field but found a " + ActiveToken.Type.ToString() + " token instead. File may be missing a value for a key");
-
-				var key = ActiveToken.Value;
-				ActiveToken = ActiveToken.Next;
-
-				if (ActiveToken == null)
-					throw new FileFormatException("Expected a Value field but found nothing, file may be incomplete or corrupted.");
-				if (ActiveToken.Type != LexerToken.Value)
-					throw new FileFormatException("Expected a Value field but found a " + ActiveToken.Type.ToString() + " token instead. File may be missing a value for a key");
-
-				var value = ActiveToken.Value.Replace("\\r", "\r").Replace("\\n", "\n");
-				ActiveToken = ActiveToken.Next;
-
-				switch(key)
+				else if (key.StartsWith("mouse_"))
 				{
-					case "translation_author":
-						Author = value;
-						break;
+					var keyCode = Convert.ToInt32(key.Substring("mouse_".Length));
+					return ((System.Windows.Forms.MouseButtons)keyCode).ToString();
 				}
-
-				FileData.Add(key, value);
-
-				Trace.WriteLine("!!!" + key + " -> '" + value + "'");
+				else return defaultValue;
 			}
-
 		}
 
-		#endregion
 	}
 }
