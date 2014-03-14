7869fe4594101d750ab0952241b1a64496960127
Michael Starkweather <Mpstark@gmail.com>
2014-03-13 22:10:26 -0500

    Added a folder to ArticulateTests to contain the tests

---
+++ ArticulateTest/Tests/SymbolTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticulateTest.Tests
+{
+    [TestFixture]
+    class SymbolTests
+    {
+        // TODO: add tests for symbols
+    }
+}
